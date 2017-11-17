2595f8c48559a14a5de3607baca5b87ad1647d7e
Rosco Pecoltran <rosco_pecoltran@msn.com>
2017-11-17 14:18:02 +0100

    update git_subtree shell script

--- .staging/bash/git_subtrees.sh
+++ .staging/bash/git_subtrees.sh
@@ -5,3 +5,12 @@ git subtree add --prefix .resources/reports/state-of-the-art-result-for-machine-
 	https://github.com/RedditSota/state-of-the-art-result-for-machine-learning-problems \
 	--squash master
 
+## Kaggle - multi-class-text-classification-cnn
+git subtree add --prefix .resources/kaggle/consumer-finance/jiegzhan/multi-class-text-classification-cnn \
+	https://github.com/jiegzhan/multi-class-text-classification-cnn \
+	--squash master
+
+## Tensorflow - image-classification-rnn
+git subtree add --prefix .resources/deeplearning/images/jiegzhan/image-classification-rnn \
+	https://github.com/jiegzhan/image-classification-rnn \
+	--squash master
