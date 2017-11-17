6390b118379304e04e4e9bab1d1bacf74379df50
Rosco Pecoltran <rosco_pecoltran@msn.com>
2017-11-17 14:50:42 +0100

    add jiegzhan's repos

--- .staging/bash/git_subtrees.sh
+++ .staging/bash/git_subtrees.sh
@@ -10,7 +10,22 @@ git subtree add --prefix .resources/kaggle/consumer-finance/jiegzhan/multi-class
 	https://github.com/jiegzhan/multi-class-text-classification-cnn \
 	--squash master
 
-## Tensorflow - image-classification-rnn
+## TensorFlow - image-classification-rnn
 git subtree add --prefix .resources/deeplearning/images/jiegzhan/image-classification-rnn \
 	https://github.com/jiegzhan/image-classification-rnn \
+	--squash master
+
+## Tensorflow - document classification
+git subtree add --prefix .resources/deeplearning/text/classification/tensorflow-font2char2word2sent2doc \
+	https://github.com/raviqqe/tensorflow-font2char2word2sent2doc \
+	--squash master
+
+## Tensorflow - Entity Recognition
+git subtree add --prefix .resources/deeplearning/text/entity-recognition/tensorflow-entrec \
+	https://github.com/raviqqe/tensorflow-entrec \
+	--squash master
+
+## Tensorflow - Deep Reinforcement Learning
+git subtree add --prefix .resources/deeplearning/learn-reinforcement/deep-rl-tensorflow \
+	https://github.com/carpedm20/deep-rl-tensorflow \
 	--squash master
