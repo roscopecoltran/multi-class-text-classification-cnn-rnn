9fa206db785c6239793d9acd087ef560844b2af7
Rosco Pecoltran <rosco_pecoltran@msn.com>
2017-11-17 14:06:52 +0100

    update dockerignore rules, add go based rest service

---
+++ .staging/bash/git_subtrees.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+## state-of-the-art-result-for-machine-learning-problems
+git subtree add --prefix .resources/reports/state-of-the-art-result-for-machine-learning-problems \
+	https://github.com/RedditSota/state-of-the-art-result-for-machine-learning-problems \
+	--squash master
+
